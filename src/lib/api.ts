635c923b0fd94daf405d5d9133acc48935fb6531
monichre <liamhellis@gmail.com>
2022-07-22 19:58:55 -0400

    added env var

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,20 +3,16 @@ async function fetchGraphQL(query) {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: "Bearer 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",
+      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
     },
     body: JSON.stringify({ query }),
   })
     .then((response) => response.json())
-    .catch((err) => {
-      console.error(err);
-    });
+    .catch((err) => {});
 }
 
 export async function getQuery(query) {
-  console.log("query: ", query);
   const entries = await fetchGraphQL(query);
-  console.log("entries: ", entries);
 
   return entries;
 }
