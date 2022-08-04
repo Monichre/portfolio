7e0ec8f4659501435d129cc82146ac75383fd127
monichre <liamhellis@gmail.com>
2022-08-04 09:54:44 -0400

    fixed typo

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -92,7 +92,6 @@ const Dashboard: React.FC<DashboardProps> = ({
                 <button style={{ opacity: 0.4 }}>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    q
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
